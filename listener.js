721d19d42440d2c907a1ce688f732451921b1395
James Poole <poole.james@gmail.com>
2015-04-06 15:59:11 -0400

    added lock check

--- listener.js
+++ listener.js
@@ -7,6 +7,11 @@ var Order = require('./models/order.js');
 var winston = require('winston');
 winston.add(winston.transports.File, { filename: 'listener.log' });
 
+
+var ReadWriteLock = require('rwlock');
+var lock = new ReadWriteLock();
+
+
 var CoinbaseExchange = require('coinbase-exchange');
 var authedClient = new CoinbaseExchange.AuthenticatedClient(Env.ACCESS_KEY, Env.SECRET_KEY, Env.PASSPHRASE_KEY, Env.REST_URL);
 var websocket = new CoinbaseExchange.WebsocketClient('BTC-USD', Env.SOCKET_URL);
@@ -25,111 +30,131 @@ winston.log('info', "Listening for messages...");
 
 websocket.on('message', function(data) {
 
-  if(data.type == 'done'){
+  if(data.type == 'done' && data.reason == 'filled'){
 
-    Order.findOne({ id : data.order_id }, function(err, foundOrder){
+    lock.writeLock(function (writeRelease) {
 
-      // Verify that we found a video with that ID
-      if(err || !foundOrder){
-         winston.log('info', "Failed to find order " + data.order_id + " with error " + err);
-         return;
-      }
+      Order.findOne({ id : data.order_id }, function(err, foundOrder){
 
-      winston.log('info', "");
-      winston.log('info', "------------------------------------------------------------");
-      winston.log('info', (new Date()).toString());
-      winston.log('info', data);
+        // Verify that we found a video with that ID
+        if(err){
+           winston.log('info', "Failed to find order " + data.order_id + " with error " + err);
+           writeRelease();
+           return;
+        }
 
-      var closedOrder = foundOrder;
-      winston.log('info', "Closed Order: " + JSON.stringify(closedOrder));
+        // If we are not tracking this order then just bail
+        if(!foundOrder){
+          writeRelease();
+          return;
+        }
 
-      if(closedOrder.side == 'buy' ){
+        winston.log('info', "");
+        winston.log('info', "------------------------------------------------------------");        
+        winston.log('info', data);
 
-        // Create a sell order at old price + $1
-        winston.log('info', "Closed buy order found.  Creating new sell order at price + 1");
+        var closedOrder = foundOrder;
+        winston.log('info', "Closed Order: " + JSON.stringify(closedOrder));
 
-        var orderToCreate = {
-          size : closedOrder.size,
-          price : Number(closedOrder.price) + ( 1 * Env.GAP_AMOUNT) ,
-          side : "sell",
-          product_id : "BTC-USD"
-        };
+        if(closedOrder.side == 'buy' ){
 
-        authedClient.sell(orderToCreate, function(error, response, data){
+          // Create a sell order at old price + $1
+          winston.log('info', "Closed buy order found.  Creating new sell order at price + 1");
 
-          if (error || response.statusCode != 200) {
-            winston.log('error', "Error creating new sell order: " + error);
-            winston.log('error', "Response: " + JSON.stringify(response));
-          }
+          var orderToCreate = {
+            size : closedOrder.size,
+            price : Number(closedOrder.price) + ( 1 * Env.GAP_AMOUNT) ,
+            side : "sell",
+            product_id : "BTC-USD"
+          };
 
-          // Add the new sell order to the existing order list
-          orderToCreate.id = data.id;
-          winston.log('info', "Created Order: " + JSON.stringify(orderToCreate));
+          authedClient.sell(orderToCreate, function(error, response, data){
 
-          Order(orderToCreate).save(function(error){
-
-            if(error){
-              winston.log('error', "Failed to create order" + orderToCreate.id + " with errro " + error);
+            if (error || response.statusCode != 200) {
+              winston.log('error', "Error creating new sell order: " + error);
+              winston.log('error', "Response: " + JSON.stringify(response));
+              writeRelease;
               return;
             }
 
-            closedOrder.remove(function(error){
+            // Add the new sell order to the existing order list
+            orderToCreate.id = data.id;
+            winston.log('info', "Created Order: " + JSON.stringify(orderToCreate));
+
+            Order(orderToCreate).save(function(error){
+
               if(error){
-                winston.log('error', "Failed to remove order" + closedOrder.id + " with errro " + error);
+                winston.log('error', "Failed to create order" + orderToCreate.id + " with errro " + error);
+                writeRelease();
                 return;
               }
+
+              closedOrder.remove(function(error){
+                if(error){
+                  winston.log('error', "Failed to remove order" + closedOrder.id + " with errro " + error);
+                  writeRelease();
+                  return;
+                }
+              });
+
             });
 
           });
 
-        });
 
+        } else {
 
-      } else {
+          // Create a buy order at old price - $1
+          winston.log('info', "Closed sell order found.  Creating new buy order at price - 1");
 
-        // Create a buy order at old price - $1
-        winston.log('info', "Closed sell order found.  Creating new buy order at price - 1");
+          var orderToCreate = {
+            size : closedOrder.size,
+            price : Number(closedOrder.price) - ( 1 * Env.GAP_AMOUNT) ,
+            side : "buy",
+            product_id : "BTC-USD"
+          };
 
-        var orderToCreate = {
-          size : closedOrder.size,
-          price : Number(closedOrder.price) - ( 1 * Env.GAP_AMOUNT) ,
-          side : "buy",
-          product_id : "BTC-USD"
-        };
+          authedClient.buy(orderToCreate, function(error, response, data){
 
-        authedClient.buy(orderToCreate, function(error, response, data){
-
-          if (error || response.statusCode != 200) {
-            winston.log('error', "Error creating new buy order: " + error);
-            winston.log('error', "Response: " + JSON.stringify(response));
-          }
-
-          // Add the new sell order to the existing order list
-          orderToCreate.id = data.id;
-          winston.log('info', "Created Order: " + JSON.stringify(orderToCreate));
-
-          Order(orderToCreate).save(function(error){
-
-            if(error){
-              winston.log('error', "Failed to create order" + orderToCreate.id + " with errro " + error);
+            if (error || response.statusCode != 200) {
+              winston.log('error', "Error creating new buy order: " + error);
+              winston.log('error', "Response: " + JSON.stringify(response));
+              writeRelease();
               return;
             }
 
-            closedOrder.remove(function(error){
+            // Add the new sell order to the existing order list
+            orderToCreate.id = data.id;
+            winston.log('info', "Created Order: " + JSON.stringify(orderToCreate));
+
+            Order(orderToCreate).save(function(error){
+
               if(error){
-                winston.log('error', "Failed to remove order" + closedOrder.id + " with errro " + error);
+                winston.log('error', "Failed to create order" + orderToCreate.id + " with errro " + error);
+                writeRelease();
                 return;
               }
+
+              closedOrder.remove(function(error){
+                if(error){
+                  winston.log('error', "Failed to remove order" + closedOrder.id + " with errro " + error);
+                  writeRelease();
+                  return;
+                }
+              });
+
             });
 
           });
 
-        });
+        }
 
-      }
+      });
 
+      writeRelease();
 
     });
+
   }
 
 });
